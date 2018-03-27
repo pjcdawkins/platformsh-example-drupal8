78319e805f097603cab18a300c4494cca2c6678c
Patrick Dawkins <pjcdawkins@gmail.com>
2018-03-27 17:05:29 -0400

    Auto-generate a drush.yml file with a valid site URL

--- web/sites/default/drushrc.php
+++ web/sites/default/drushrc.php
@@ -3,20 +3,9 @@
  * Example Drush configuration file for a Platform.sh Drupal site.
  */
 
-if (PHP_SAPI === 'cli' && getenv('PLATFORM_ROUTES') && getenv('PLATFORM_APPLICATION_NAME')) {
-  $routes = json_decode(base64_decode(getenv('PLATFORM_ROUTES')), TRUE);
-  $expected_route_urls = [
-    'https://{default}/',
-    'https://www.{default}/',
-    'http://{default}/',
-    'http://www.{default}/',
-  ];
-  foreach ($routes as $url => $route) {
-    if ($route['type'] === 'upstream'
-    	&& $route['upstream'] === getenv('PLATFORM_APPLICATION_NAME')
-      && in_array($route['original_url'], $expected_route_urls)) {
-      $options['uri'] = $url;
-      break;
-    }
+if (PHP_SAPI === 'cli' && getenv('PLATFORM_APP_DIR')) {
+  require getenv('PLATFORM_APP_DIR') . '/drush/platformsh_drush.inc';
+  if ($siteUrl = _platformsh_drush_site_url()) {
+    $options['uri'] = $siteUrl;
   }
 }
